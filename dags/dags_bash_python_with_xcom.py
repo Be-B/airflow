795ba497f6895bcc26f0e9e8004e1dce6e4fa6a4
Munkyo <dlansry77@naver.com>
2025-04-14 22:42:58 +0900

    pytho_bash_xcom dags 재수정 업로드

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -10,9 +10,9 @@ with DAG(
     catchup=False,
 ) as dag:
 
-    @task(task_id ='python push')
+    @task(task_id='python_push')
     def python_push_xcom():
-        result_dict = {'status':' Good', 'data':[1,2,3],'options_cnt':100}
+        result_dict = {'status':'Good', 'data':[1,2,3],'options_cnt':100}
         return result_dict
 
     bash_pull = BashOperator(
@@ -24,8 +24,7 @@ with DAG(
         },
         bash_command='echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
     )
-    python_push_xcom >> bash_pull
-
+    python_push_xcom() >> bash_pull
 
     bash_push = BashOperator(
     task_id='bash_push',
@@ -37,11 +36,9 @@ with DAG(
     @task(task_id='python_pull')
     def python_pull_xcom(**kwargs):
         ti = kwargs['ti']
-        # 키 값으로 pull하여 status_value에 전달한다.
         status_value = ti.xcom_pull(key='bash_pushed')
-        # task_ids로 가져오면 해당 task의 return 값을 들고오게 되므로 PUSH_COMPLETE가 나옴
         return_value = ti.xcom_pull(task_ids='bash_push')
         print('status_value:' + str(status_value))
         print('return_value:' + return_value)
 
-    bash_push >> python_pull_xcom
+    bash_push >> python_pull_xcom()
