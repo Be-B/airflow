1fcc011dd21a559d960603158752975b2ea6bf1a
Munkyo <dlansry77@naver.com>
2025-04-14 22:29:36 +0900

    pytho_bash_xcom dags 업로드

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -14,6 +14,8 @@ with DAG(
         task_id='bash_push',
         bash_command='echo START && '
                      'echo XCOM_PUSHED '
+                     # bash 오퍼레이터에서는 템플릿 문법으로 작성해야함
+                     # 파이썬에서는 args, kwargs로 작성함
                      '{{ ti.xcom_push(key="bash_pushed", value="first_bash_message") }} && '
                      'echo COMPLETE'
     )
@@ -23,6 +25,8 @@ with DAG(
         env= {'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
               'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
         bash_command='echo $PUSHED_VALUE && echo $RETURN_VALUE ',
+
+        # 마지막 bash_command가 xcom에 올라가지 않도록 코드를 줌
         do_xcom_push=False
     )
 
